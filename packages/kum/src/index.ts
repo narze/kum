c64e6dce2d6aa357201a19d34e40a95f6a667738
Manassarn Manoonchai <manassarn@gmail.com>
2022-07-05 00:31:33 +0700

    feat: use fsm

--- packages/kum/src/index.ts
+++ packages/kum/src/index.ts
@@ -1,6 +1,107 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('dev only output');
+export interface Kum {
+  head: string
+  vowel: string | null
+  tail: string | null
+}
+
+// TODOS
+// - ร หัน: กรรม
+// - สระเอีย
+// - ไม้หันอากาศ
+// - Special words
+// - etc.
+
+export class Kum {
+  constructor(word: string) {
+    const graphemes = word.split('')
+    this.head = ''
+    this.tail = ''
+
+    let char
+    let ptr = 0
+    let mode
+
+    while ((char = graphemes[ptr])) {
+      if (char.match(/[เแโใไ]/)) {
+        // Front vowels
+        this.vowel = char + 'อ'
+      } else if (char.match(/[ิีึืั]/) || char.match(/[ุู]/)) {
+        // Top / Bottom vowels
+        mode = 'tail'
+        if (this.tail.length) {
+          // Empty tail
+          this.head += this.tail
+          this.tail = ''
+        }
+        this.vowel = 'อ' + char
+      } else if (char.match(/[ะาำ]/)) {
+        // Back vowels
+        mode = 'tail'
+        if (this.tail.length) {
+          // Empty tail
+          this.head += this.tail
+          this.tail = ''
+        }
+        this.vowel = 'อ' + char
+      } else if (char.match(/[ก-ฮ]/)) {
+        if (mode == 'tail') {
+          this.tail += char
+        } else {
+          const h = this.head.slice(-1)
+
+          if (!h) {
+            this.head += char
+          } else if (isKuab(`${h}${char}`) || isNum(`${h}${char}`)) {
+            // อักษรนำ / ควบกล้ำ
+
+            this.head += char
+          } else {
+            this.tail += char
+          }
+        }
+      }
+      // }
+
+      // Terminate
+
+      if (char.match(/[ะำ]/)) {
+        break
+      }
+
+      ptr++
+    }
+    // Head
+    // this.head = word.match(/[ก-ฮ]+/)![0];
+    if (!this.tail) {
+      this.tail = null
+    }
   }
-  return a + b;
-};
+}
+
+export const fromWord = (word: string): Kum => {
+  return new Kum(word)
+}
+
+function isKuab(chars: string): boolean {
+  if (chars[1].match(/[รลว]/)) {
+    return true
+  }
+
+  if (chars.match(/(ทร|ทย)/)) {
+    return true
+  }
+
+  return false
+}
+
+function isNum(chars: string): boolean {
+  if (chars[0] == 'ห' && chars[1].match(/[ยรลวงญณนม]/)) {
+    return true
+  }
+
+  if (chars == 'อย') {
+    return true
+  }
+
+  return false
+}
--- packages/kum/test/index.test.ts
+++ packages/kum/test/index.test.ts
@@ -1,7 +1,65 @@
-import { sum } from '../src/index';
+import { fromWord } from '../src/index'
 
-describe('sum', () => {
-  it('adds two numbers together', () => {
-    expect(sum(1, 1)).toEqual(2);
-  });
-});
+describe('fromWord', () => {
+  it('exists', () => {
+    expect(fromWord).toBeDefined()
+  })
+
+  describe('คำ', () => {
+    it('extracts to ค + ำ', () => {
+      const kum = fromWord('คำ')
+
+      expect(kum.head).toEqual('ค')
+      expect(kum.vowel).toEqual('อำ')
+    })
+  })
+
+  describe('ไหน', () => {
+    it('extracts to หน + ไอ', () => {
+      const kum = fromWord('ไหน')
+
+      expect(kum.head).toEqual('หน')
+      expect(kum.vowel).toEqual('ไอ')
+    })
+  })
+
+  // describe('ไทย', () => {
+  //   it('extracts to ท + ไอ + ย', () => {
+  //     const kum = fromWord('ไทย');
+
+  //     expect(kum.head).toEqual('ท');
+  //     expect(kum.vowel).toEqual('ไอ');
+  //     expect(kum.tail).toEqual('ย');
+  //   });
+  // });
+
+  describe('หมุด', () => {
+    it('extracts to หม + อุ + ด', () => {
+      const kum = fromWord('หมุด')
+
+      expect(kum.head).toEqual('หม')
+      expect(kum.vowel).toEqual('อุ')
+      expect(kum.tail).toEqual('ด')
+    })
+  })
+
+  describe('กละ', () => {
+    it('extracts to กล + ะ', () => {
+      const kum = fromWord('กละ')
+
+      expect(kum.head).toEqual('กล')
+      expect(kum.vowel).toEqual('อะ')
+      expect(kum.tail).toEqual(null)
+    })
+  })
+
+  describe('สบาย', () => {
+    it('extracts to สบ + า + ย', () => {
+      const kum = fromWord('สบาย')
+
+      expect(kum.head).toEqual('สบ')
+      expect(kum.vowel).toEqual('อา')
+      expect(kum.tail).toEqual('ย')
+    })
+  })
+})
